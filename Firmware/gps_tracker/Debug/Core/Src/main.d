3d488d5abe61c7b5d3a6c1ffd2bf79ee1f3cd38d
Saie12 <saieshsasane@gmail.com>
2025-11-30 20:02:07 +0530

    Phase 7: SIM808 error handling, timeouts, and retries

--- Firmware/gps_tracker/Debug/Core/Src/main.d
+++ Firmware/gps_tracker/Debug/Core/Src/main.d
@@ -27,7 +27,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Core/Inc/sim808_config.h ../Core/Inc/sim808_driver.h \
  ../Core/Inc/sim808_config.h ../Core/Inc/sim808_gps.h \
  ../Core/Inc/sim808_gprs.h ../Core/Inc/sim808_driver.h \
- ../Core/Inc/sim808_power.h
+ ../Core/Inc/sim808_power.h ../Core/Inc/gps_parser.h \
+ ../Core/Inc/gps_uart.h
 ../Core/Inc/main.h:
 C:/Users/91755/Documents/PCB\ Design/STM32\ GPS\ Tracker/Firmware/gps_tracker/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
 C:/Users/91755/Documents/PCB\ Design/STM32\ GPS\ Tracker/Firmware/gps_tracker/Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_conf.h:
@@ -62,3 +63,5 @@ C:/Users/91755/Documents/PCB\ Design/STM32\ GPS\ Tracker/Firmware/gps_tracker/Dr
 ../Core/Inc/sim808_gprs.h:
 ../Core/Inc/sim808_driver.h:
 ../Core/Inc/sim808_power.h:
+../Core/Inc/gps_parser.h:
+../Core/Inc/gps_uart.h:
